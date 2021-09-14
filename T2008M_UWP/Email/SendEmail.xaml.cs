6062e1aa92771a0df154bb3709dd8f9ce44d86b9
lethanhdat25 <70951430+lethanhdat25@users.noreply.github.com>
2021-09-14 16:06:34 +0700

    upcode

---
+++ T2008M_UWP/Email/SendEmail.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace T2008M_UWP.Email
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SendEmail : Page
+    {
+        public SendEmail()
+        {
+            this.InitializeComponent();
+        }
+        int index = 1;
+        List<String> ListLetter = new List<String>();
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ListLetter.Add(inputEmail.Text + " - " + inputTopic.Text + " - " + inputContent.Text.Substring(0, 10));
+            letters.Text += index + " - " +ListLetter.Last() + "\n";
+            inputTopic.Text = "";
+            inputEmail.Text = "";
+            inputContent.Text = "";
+            index++;
+        }
+    }
+}
